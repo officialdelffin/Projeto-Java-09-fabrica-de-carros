b68b9ee70defde8e34202e99794aaf08edff2254
officialdelffin <workdelffin@gmail.com>
2026-01-19 21:17:21 -0300

    commit 1

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+/* Projeto : Fabrica de carro
+
+ Fábrica de Carros em Java para consolidar conceitos de herança, polimorfismo e sobrescrita de métodos. O sistema simula a produção de diferentes tipos de veículos a partir de uma classe base, utilizando polimorfismo
+ para especializar comportamentos e sobrescrita para funcionalidades específicas de cada modelo. Foco em POO e reúso de código. */
+
+
+// Classe de execução do sistema : :
+public class Main {
+
+
+    // Metodo que executa o sistema :
+    public static void main( String[] args ) {
+
+
+
+
+
+    }
+
+
+}
