44df76de5bfe93a5c2f15473e80217a19ac6dc34
officialdelffin <workdelffin@gmail.com>
2026-01-19 21:28:14 -0300

    commit 2

---
+++ src/br/com/officialdelffin/CarMold/CarMold.java
@@ -0,0 +1,39 @@
+package br.com.officialdelffin.CarMold;
+
+
+// Classe que é o molde para todos os carros :
+public class CarMold {
+
+
+    // Atributos :
+
+    // Atributos de todos os carros :
+    protected String name         = "";
+    protected String color        = "";
+    protected  int yearOfRelease  = 0;
+
+
+    // Métodos :
+
+    // Metodo que faz o carro acelerar :
+    protected void accelerate () {
+
+
+
+
+
+    }
+
+
+    // Metodo que faz o carro freiar :
+    protected void brake () {
+
+
+
+
+
+    }
+
+
+
+}
